5851f14817bcd8ee1ef6b51d651042be8203a355
Nirmal Thewarathanthri <nirmal@live.com>
2019-09-02 07:29:18 +0800

    deployment script updated

--- Student/DeployInfra.ps1
+++ Student/DeployInfra.ps1
@@ -11,16 +11,24 @@
 # Install-Module -Name AzureRM -Force -Scope CurrentUser -AllowClobber
 
 #Step 1: Use a name no longer then five charactors all lowercase.  Your initials would work well if working in the same sub as others.
-$mlabname = 'your-initials-here-in-lowercase'
+#$mlabname = 'your-initials-here-in-lowercase'
+$mlabname = 'nt'
+$vmusername = "azureadmin"
 $randomstg = Get-Random -Maximum 99999999
+$randomVMname = $mlabname.ToLower()+$randomstg
+$rgname = ""
 
 #Resource Group
-New-AzResourceGroup -Name "rg-"+$mlabname.tolower()+$randomstg -Location 'Australia East' #The hack uses a few features in preview so its best to leave this in East US
+#New-AzResourceGroup -Name "rg-"+$mlabname.tolower()+$randomstg -Location 'Australia East' 
 $rg = get-Azresourcegroup -Name ("rg-"+$mlabname.tolower()+$randomstg)
+$vmpassword = Read-Host -Prompt "Provide a password"
 
 #Infra Resource Deployment
-New-AzResourceGroupDeployment -Name "rgdep"+$mlabname.tolower()+$randomstg
+$paramObject = @{ 'virtualMachineName' = $randomVMname
+                  'adminUsername' = $vmusername 
+                  'vmpassword' = $vmpassword
+                    }
+New-AzResourceGroupDeployment -TemplateUri '' -TemplateParameterObject $paramObject
 
 #GetDate
-$((get-date).ToLocalTime()).ToString("ddMMyyHHmmss")
-
+#$((get-date).ToLocalTime()).ToString("ddMMyyHHmmss")
