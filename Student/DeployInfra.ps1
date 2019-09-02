5444fd3dc5d8547d0480bf1c1a6fbd2d62ea5264
Nirmal Thewarathanthri <nirmal@live.com>
2019-09-02 08:32:03 +0800

    addedsubname and rgname

--- Student/DeployInfra.ps1
+++ Student/DeployInfra.ps1
@@ -12,23 +12,26 @@
 
 #Step 1: Use a name no longer then five charactors all lowercase.  Your initials would work well if working in the same sub as others.
 #$mlabname = 'your-initials-here-in-lowercase'
+Select-AzSubscription -SubscriptionName "SubscriptionName"
+
 $mlabname = 'nt'
 $vmusername = "azureadmin"
 $randomstg = Get-Random -Maximum 99999999
 $randomVMname = $mlabname.ToLower()+$randomstg
-$rgname = ""
+$rgname = "s32mon112"
+
 
 #Resource Group
 #New-AzResourceGroup -Name "rg-"+$mlabname.tolower()+$randomstg -Location 'Australia East' 
-$rg = get-Azresourcegroup -Name ("rg-"+$mlabname.tolower()+$randomstg)
-$vmpassword = Read-Host -Prompt "Provide a password"
+$rg = get-Azresourcegroup -Name $rgname 
+$vmpassword = ConvertTo-SecureString (Read-Host -Prompt "Provide a password") -AsPlainText -Force
 
 #Infra Resource Deployment
 $paramObject = @{ 'virtualMachineName' = $randomVMname
                   'adminUsername' = $vmusername 
-                  'vmpassword' = $vmpassword
-                    }
-New-AzResourceGroupDeployment -TemplateUri '' -TemplateParameterObject $paramObject
+                  }
+New-AzResourceGroupDeployment -TemplateUri 'https://github.com/nthewara/azuremonitorhackathon/raw/master/InfraDeployment.json' -TemplateParameterObject $paramObject -adminPassword $vmpassword -ResourceGroupName $rgname 
 
 #GetDate
 #$((get-date).ToLocalTime()).ToString("ddMMyyHHmmss")
+
